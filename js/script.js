4d635b71c73e2a3b03538796264cac2e8e844e56
DaniloAugustoluz <daniloaugusto.luzz@gmail.com>
2024-05-08 23:05:08 -0300

    ProjetoConcluido

---
+++ js/script.js
@@ -0,0 +1,313 @@
+function redirect() {
+    window.location.href = 'depoimentos.html';
+}
+
+function redirectVoltar() {
+    window.location.href = 'index.html';
+}
+
+/*      MODAL COMPLETO      */
+const leticiaOpenModal = document.querySelector('#open-modal');
+const leticiacloseModal = document.querySelector('#close');
+const leticiaModal = document.querySelector('#modal');
+const leticiafade = document.querySelector('#fade');
+
+const LeticiatoggleModal = () => {
+    leticiaModal.classList.toggle('hide');
+    leticiafade.classList.toggle('hide');
+}
+
+[leticiaOpenModal, leticiacloseModal, leticiafade].forEach((elemento) => {
+    elemento.addEventListener('click', () => LeticiatoggleModal());
+    
+});
+
+
+
+/*      MODAL COMPLETO MARIA    */
+const openModalMaria = document.querySelector('#open-modalMaria');
+const closeModalMaria = document.querySelector('#closeMaria');
+const modal = document.querySelector('#modalMaria');
+const fadeMaria = document.querySelector('#fadeMaria');
+
+const toggleModalMaria = () => {
+    modal.classList.toggle('hide');
+    fade.classList.toggle('hide');
+}
+
+[openModalMaria, closeModalMaria, fadeMaria].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalMaria());  
+});
+
+
+/*      MODAL COMPLETO TAYNARA    */
+const openModalTaynara = document.querySelector('#open-modalTaynara');
+const closeModalTaynara = document.querySelector('#closeTaynara');
+const modalTaynara = document.querySelector('#modalTaynara');
+const fadeTaynara = document.querySelector('#fadeTaynara');
+
+const toggleModalTaynara = () => {
+    modalTaynara.classList.toggle('hide');
+    fadeTaynara.classList.toggle('hide');
+}
+
+[openModalTaynara, closeModalTaynara, fadeTaynara].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalTaynara());  
+});
+
+
+/*      MODAL COMPLETO KARINA    */
+const openModalKarina = document.querySelector('#open-modalKarina');
+const closeModalKarina = document.querySelector('#closeKarina');
+const modalKarina = document.querySelector('#modalKarina');
+const fadeKarina = document.querySelector('#fadeKarina');
+
+const toggleModalKarina = () => {
+    modalKarina.classList.toggle('hide');
+    fadeKarina.classList.toggle('hide');
+}
+
+[openModalKarina, closeModalKarina, fadeKarina].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalKarina());  
+});
+
+
+/*      MODAL COMPLETO ALINE    */
+const openModalAline = document.querySelector('#open-modalAline');
+const closeModalAline = document.querySelector('#closeAline');
+const modalAline = document.querySelector('#modalAline');
+const fadeAline = document.querySelector('#fadeAline');
+
+const toggleModalAline = () => {
+    modalAline.classList.toggle('hide');
+    fadeAline.classList.toggle('hide');
+}
+
+[openModalAline, closeModalAline, fadeAline].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalAline());  
+});
+
+
+
+/*      MODAL COMPLETO EDI    */
+const openModalEdi = document.querySelector('#open-modalEdi');
+const closeModalEdi = document.querySelector('#closeEdi');
+const modalEdi = document.querySelector('#modalEdi');
+const fadeEdi = document.querySelector('#fadeEdi');
+
+const toggleModalEdi = () => {
+    modalEdi.classList.toggle('hide');
+    fadeEdi.classList.toggle('hide');
+}
+
+[openModalEdi, closeModalEdi, fadeEdi].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalEdi());  
+});
+
+
+/*      MODAL COMPLETO MELISSA    */
+const openModalMelissa = document.querySelector('#open-modalMelissa');
+const closeModalMelissa = document.querySelector('#closeMelissa');
+const modalMelissa = document.querySelector('#modalMelissa');
+const fadeMelissa = document.querySelector('#fadeMelissa');
+
+const toggleModalMelissa = () => {
+    modalMelissa.classList.toggle('hide');
+    fadeMelissa.classList.toggle('hide');
+}
+
+[openModalMelissa, closeModalMelissa, fadeMelissa].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalMelissa());  
+});
+
+
+
+/*      MODAL COMPLETO SABRINA    */
+const openModalSabrina = document.querySelector('#open-modalSabrina');
+const closeModalSabrina = document.querySelector('#closeSabrina');
+const modalSabrina = document.querySelector('#modalSabrina');
+const fadeSabrina = document.querySelector('#fadeSabrina');
+
+const toggleModalSabrina = () => {
+    modalSabrina.classList.toggle('hide');
+    fadeSabrina.classList.toggle('hide');
+}
+
+[openModalSabrina, closeModalSabrina, fadeSabrina].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalSabrina());  
+});
+
+
+/*      MODAL COMPLETO ARIANE    */
+const openModalAriane = document.querySelector('#open-modalAriane');
+const closeModalAriane = document.querySelector('#closeAriane');
+const modalAriane = document.querySelector('#modalAriane');
+const fadeAriane = document.querySelector('#fadeAriane');
+
+const toggleModalAriane = () => {
+    modalAriane.classList.toggle('hide');
+    fadeAriane.classList.toggle('hide');
+}
+
+[openModalAriane, closeModalAriane, fadeAriane].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalAriane());  
+});
+
+
+
+
+/*      MODAL COMPLETO ANA PAULA    */
+const openModalAna = document.querySelector('#open-modalAna');
+const closeModalAna = document.querySelector('#closeAna');
+const modalAna = document.querySelector('#modalAna');
+const fadeAna = document.querySelector('#fadeAna');
+
+const toggleModalAna = () => {
+    modalAna.classList.toggle('hide');
+    fadeAna.classList.toggle('hide');
+}
+
+[openModalAna, closeModalAna, fadeAna].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalAna());  
+});
+
+
+
+/*      MODAL COMPLETO D. PRISCILA    */
+const openModalPriscila = document.querySelector('#open-modalPriscila');
+const closeModalPriscila = document.querySelector('#closePriscila');
+const modalPriscila = document.querySelector('#modalPriscila');
+const fadePriscila = document.querySelector('#fadePriscila');
+
+const toggleModalPriscila = () => {
+    modalPriscila.classList.toggle('hide');
+    fadePriscila.classList.toggle('hide');
+}
+
+[openModalPriscila, closeModalPriscila, fadePriscila].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalPriscila());  
+});
+
+
+
+/*      MODAL COMPLETO MÔNICA   */
+const openModalMonica = document.querySelector('#open-modalMonica');
+const closeModalMonica = document.querySelector('#closeMonica');
+const modalMonica = document.querySelector('#modalMonica');
+const fadeMonica = document.querySelector('#fadeMonica');
+
+const toggleModalMonica = () => {
+    modalMonica.classList.toggle('hide');
+    fadeMonica.classList.toggle('hide');
+}
+
+[openModalMonica, closeModalMonica, fadeMonica].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalMonica());  
+});
+
+
+
+
+/*      MODAL COMPLETO BRUNA   */
+const openModalBruna = document.querySelector('#open-modalBruna');
+const closeModalBruna = document.querySelector('#closeBruna');
+const modalBruna = document.querySelector('#modalBruna');
+const fadeBruna = document.querySelector('#fadeBruna');
+
+const toggleModalBruna = () => {
+    modalBruna.classList.toggle('hide');
+    fadeBruna.classList.toggle('hide');
+}
+
+[openModalBruna, closeModalBruna, fadeBruna].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalBruna());  
+});
+
+
+
+
+/*      MODAL COMPLETO JADY   */
+const openModalJady = document.querySelector('#open-modalJady');
+const closeModalJady = document.querySelector('#closeJady');
+const modalJady = document.querySelector('#modalJady');
+const fadeJady = document.querySelector('#fadeJady');
+
+const toggleModalJady = () => {
+    modalJady.classList.toggle('hide');
+    fadeJady.classList.toggle('hide');
+}
+
+[openModalJady, closeModalJady, fadeJady].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalJady());  
+});
+
+
+
+
+/*      MODAL COMPLETO BIANCA   */
+const openModalBianca = document.querySelector('#open-modalBianca');
+const closeModalBianca = document.querySelector('#closeBianca');
+const modalBianca = document.querySelector('#modalBianca');
+const fadeBianca = document.querySelector('#fadeBianca');
+
+const toggleModalBianca = () => {
+    modalBianca.classList.toggle('hide');
+    fadeBianca.classList.toggle('hide');
+}
+
+[openModalBianca, closeModalBianca, fadeBianca].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalBianca());  
+});
+
+
+
+/*      MODAL COMPLETO TALITA   */
+const openModalTalita = document.querySelector('#open-modalTalita');
+const closeModalTalita = document.querySelector('#closeTalita');
+const modalTalita = document.querySelector('#modalTalita');
+const fadeTalita = document.querySelector('#fadeTalita');
+
+const toggleModalTalita = () => {
+    modalTalita.classList.toggle('hide');
+    fadeTalita.classList.toggle('hide');
+}
+
+[openModalTalita, closeModalTalita, fadeTalita].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalTalita());  
+});
+
+
+
+
+/*      MODAL COMPLETO JÉSSICA   */
+const openModalJessica = document.querySelector('#open-modalJessica');
+const closeModalJessica = document.querySelector('#closeJessica');
+const modalJessica = document.querySelector('#modalJessica');
+const fadeJessica = document.querySelector('#fadeJessica');
+
+const toggleModalJessica = () => {
+    modalJessica.classList.toggle('hide');
+    fadeJessica.classList.toggle('hide');
+}
+
+[openModalJessica, closeModalJessica, fadeJessica].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalJessica());  
+});
+
+
+
+
+/*      MODAL COMPLETO MARIA FERNANDA   */
+const openModalMariaF = document.querySelector('#open-modalMariaF');
+const closeModalMariaF = document.querySelector('#closeMariaF');
+const modalMariaF = document.querySelector('#modalMariaF');
+const fadeMariaF = document.querySelector('#fadeMariaF');
+
+const toggleModalMariaF = () => {
+    modalMariaF.classList.toggle('hide');
+    fadeMariaF.classList.toggle('hide');
+}
+
+[openModalMariaF, closeModalMariaF, fadeMariaF].forEach((elemento) => {
+    elemento.addEventListener('click', () => toggleModalMariaF());  
+});
